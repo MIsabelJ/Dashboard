05d59a91dac7c9e43aafa7f2492dcfc4e4a2f1a3
agustinm.itcs <agustinm@itcsoluciones.com>
2024-06-05 10:47:22 -0300

    fix: arreglado el maldito typo de login.css

--- src/components/pages/Login/Login.tsx
+++ src/components/pages/Login/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Form } from 'react-bootstrap';
-import './Login.css';
+import './login.css';
 
 const Login = () => {
   const [showPass, setShowPass] = useState(false);
